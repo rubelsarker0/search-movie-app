4211fd204d8c2111c7f237853e76a05e28b002cd
rubelsarker0 <rubelsarker0@outlook.com>
2021-08-16 18:14:16 +0100

    added-search functionality

--- js/movie.js
+++ js/movie.js
@@ -1,27 +1,36 @@
-// 1. api call
-//2. loop thorough all the data get from API
-//3. get all the details from thr data
-// Add the image in the first colum n
-//Add the details in the 2nd column
-
-// let title, year, release;
-// title = document.getElementById('title');
-// year = document.getElementById('year');
-// release = document.getElementById('release');
-
-// select the search input field
-// const searchBtn = document.getElementById('search-btn');
+const moviePoster = document.getElementById('movie-poster');
+const title = document.getElementById('title');
+const year = document.getElementById('year');
+const released = document.getElementById('released');
+const genre = document.getElementById('genre');
+const director = document.getElementById('director');
+const plot = document.getElementById('plot');
+const ratings = document.getElementById('ratings');
 const apiKey = '4efdd0b3';
 
 document.getElementById('search-btn').addEventListener('click', async () => {
-	const searchInput = document.getElementById('search-input').value;
+	const searchField = document.getElementById('search-input');
+	let searchInput = searchField.value;
 
 	// omdb api call to bring the movie
 	const response = await axios.get(
 		`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchInput}`
 	);
 	const movie = response.data;
-	console.log(movie);
+	const { Title, Year, Released, Genre, Director, Plot, Poster, Ratings } =
+		movie;
+
+	//show movie poster
+	moviePoster.setAttribute('src', Poster);
+	moviePoster.classList.remove('d-none');
+
+	//Add movie details
+	title.textContent = `${Title}`;
+	year.textContent = `Year: ${Year}`;
+	released.textContent = `Released: ${Released}`;
+	genre.textContent = `Genre: ${Genre}`;
+	director.textContent = `Director: ${Director}`;
+	plot.textContent = `Plot: ${Plot}`;
 
 	// clear the search input field
 	searchInput.value = '';
